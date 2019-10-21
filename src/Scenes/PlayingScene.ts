e04b70c49a8bfb25735236250721f934a58224ef
Esteban <estebansalazar498@gmail.com>
2019-10-21 17:26:53 -0500

    branch

--- src/Scenes/PlayingScene.ts
+++ src/Scenes/PlayingScene.ts
@@ -1,11 +1,41 @@
 import Scene from "./../Scene";
 import Engine from './../Engine';
 import MainMenuScene from './MainMenuScene';
+import GameObject from '../GameObject';
+import GameContext from '../GameContext';
+import Enemies from '../Enemies';
 
 class PlayingScene extends Scene{
 
+    private enemies: Enemies[];
+    private numberOfEnemies: number = 30;
+
 
     public  render = () => {
+
+        const {context} = GameContext;
+        const {width , height} = GameContext.context.canvas;
+
+        
+
+        for (let index = 0; index < this.enemies.length; index++) {
+            const element = this.enemies[index];
+            this.enemies.push(new Enemies());
+            
+        }
+
+
+
+        context.save();
+        context.beginPath();
+        context.fillStyle = "black";
+        context.fillRect(0,0, width , height);
+        context.closePath();
+        context.restore();
+
+
+
+
     };
     public enter = () =>{
     };
